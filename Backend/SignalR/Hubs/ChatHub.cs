b7f6c108763f1a772eb1454cbaa6efa902307949
WhatzGames <WhatzGames@users.noreply.github.com>
2023-02-21 22:55:16 +0100

    Initial setup for backend projects

---
+++ Backend/SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Hubs;
+
+public sealed class ChatHub : Hub<string>
+{
+    
+}
---
+++ Backend/SignalR/Program.cs
@@ -0,0 +1,8 @@
+using SignalR.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+var app = builder.Build();
+app.MapHub<ChatHub>("/chat");
+
+app.Run();
